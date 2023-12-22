a72f2663b39750f458305e27218739b6c5e41588
CarinWood <carin.wood.85@gmail.com>
2023-12-22 16:46:20 +0100

    update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,14 @@
 import './card.css'
+import plant from '../../assets/images/plant.svg'
 
 // eslint-disable-next-line react/prop-types
 export const Card = ({name, title, date, img}) => {
   return (
     <div className='card'>
-        <img src={img} alt="title" className='plant-img'/>
+        <div className='image-container'>
+            <img src={plant} alt="plant" className='plant-logo'/>
+            <img src={img} alt={title} className='plant-image'/>
+        </div>
         <p>{name}</p>
         <p>{title}</p>
         <p>{date}</p>
