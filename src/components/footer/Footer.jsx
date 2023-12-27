9f36d89915563a613c6456752a88a6bb7961648c
CarinWood <carin.wood.85@gmail.com>
2023-12-27 13:08:48 +0100

    added icons in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import './footer.css'
 import logo from '../../assets/images/Logo.svg'
+import { MdOutlinePhone } from "react-icons/md";
+import { PiEnvelope } from "react-icons/pi";
 
 export const Footer = () => {
   return (
@@ -8,6 +10,8 @@ export const Footer = () => {
                 <div className='black'>
                         <p className='footer-text'>Vi hörs!</p>
                         <div className='shade'></div>
+                        <MdOutlinePhone className='phone-icon'/>
+                        <PiEnvelope className='envelope-icon'/>
                         <p className='phone'>0701-234567</p>
                         <p className='email'>mail@mail.se</p>
                 </div>
