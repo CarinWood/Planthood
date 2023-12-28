2e2e2947da483e95cac7a93a6a7eabde4da65555
CarinWood <carin.wood.85@gmail.com>
2023-12-28 13:38:58 +0100

    my pages hero responsive css finished

--- src/components/mypagesHero/MyPagesHero.jsx
+++ src/components/mypagesHero/MyPagesHero.jsx
@@ -1,11 +1,14 @@
 import './myPagesHero.css'
 import logo from '../../assets/images/Logo.svg'
+import { GiHamburgerMenu } from "react-icons/gi";
 
 export const MyPagesHero = () => {
   return (
     <div className='pages-hero'>
         <img src={logo} alt="logo" className='my-pages-logo' />
         <p>Mina sidor</p>
+        <div className='black-bg'></div>
+        <GiHamburgerMenu className='menubar'/>
 
     </div>
   )
